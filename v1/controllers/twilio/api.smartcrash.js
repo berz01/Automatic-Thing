4427289d5be145269e7ea9a8ee1e4fb4e54214fe
ereio <Tte11@my.fsu.edu>
2016-12-21 13:02:12 -0500

    Revert "- made text changes, taylor ur a scrub"
    This reverts commit 757133221e4e08ea7286f46f2cab1d1db1e6c88d.

--- v1/controllers/twilio/api.smartcrash.js
+++ v1/controllers/twilio/api.smartcrash.js
@@ -5,7 +5,7 @@ var accountSid = 'ACc0eb88b3db3d429fc7aa9c9f8a018d26'; // Your Account SID from 
 var authToken = '342368f85e36b5174b5cdcb87e98a56e'; // Your Auth Token from www.twilio.com/console
 var smartcrash = require('./module.smartcrash');
 var twilioClient = new twilio.RestClient(accountSid, authToken);
-var defaultCrashMessage = "Our algorithmn has detected you have been in a wreck. Please respond with a Yes if you have been in a wreck, No if you havent, or Emergency";
+var defaultCrashMessage = "Our algorithmn has detected you have been in a wreck. Please respond with a yes if you have been in a wreck, No if you havent, and Emergency if you need our HERO truck to come by";
 
 var clientSms = {
     numberOfCustomer: "+14043077465"
@@ -41,7 +41,7 @@ exports.incomingSms = function(req, res) {
 
 exports.crashDetection = function(req, res){
     if(req.body.crash){
-      clientSms.numberOfCustomer = req.body.number != null ? req.body.number : clientSms.numberOfCustomer;
+      clientSms.numberOfCustomer = req.body.number != null ? req.body.number : clientSms.numberOfCustomer; 
       smartcrash.sendSms(clientSms.numberOfCustomer, defaultCrashMessage);
       return res.send('Successful');
     } else {
